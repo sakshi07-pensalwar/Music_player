3783b61b1e62997cd2bc654e4ad3a0896ea7796b
sakshi pensalwar <155842386+sakshi07-pensalwar@users.noreply.github.com>
2024-08-13 16:48:10 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,66 @@
+
+
+let allMusic = [
+  {
+    name: "Kuch Baatein",
+    artist: "Jubin Nautiyal, Payal Dev",
+    img: "1",
+    src: "1"
+  },
+  {
+    name: "Barsaat Ki Dhun",
+    artist: "Jubin Nautiyal",
+    img: "4",
+    src: "4"
+  },
+  {
+    name: "Tumse Pyaar Karke",
+    artist: "Jubin Nautiyal, Payal Dev",
+    img: "2",
+    src: "2"
+  },
+  {
+    name: "Rim Jhim",
+    artist: "Ami Mishra, Jubin Nautiyal",
+    img: "3",
+    src: "3"
+  },
+  {
+    name: "Jug Jug Jeeve",
+    artist: "Sachet Tandon, Parampara Tandon, Sachin-Jigar",
+    img: "5",
+    src: "5"
+  },
+  {
+    name: "Meri Zindagi Hai Tu",
+    artist: "Jubin Nautiyal and Neeti Mohan",
+    img: "6",
+    src: "6"
+  },
+  {
+    name: "Tujh Mein Rab Dikhta Hai",
+    artist: "Roopkumar Rathod",
+    img: "7",
+    src: "7"
+  },
+  {
+    name: "Dil Ko Karaar Aaya",
+    artist: "Yasser Desai, Neha Kakkar",
+    img: "8",
+    src: "8"
+  },
+  {
+    name: "Chale Aana",
+    artist: "Amaal Mallik, Armaan Malik",
+    img: "9",
+    src: "9"
+  },
+  {
+    name: "Saiyaara",
+    artist: "Mohit Chauhan, Tarannum Mallik",
+    img: "10",
+    src: "10"
+  },
+  
+  
+];
